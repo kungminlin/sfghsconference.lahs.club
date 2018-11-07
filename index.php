b0a2227f49dfee469e3a67170d6ad56e6b3d0cfe
kungminlin <mml0816@gmail.com>
2018-11-07 20:45:37 +0000

    init files

---
+++ index.php
@@ -0,0 +1,521 @@
+<!DOCTYPE HTML>
+<!--
+	Dimension by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>LAHS Green Team</title>
+		<link rel="shortcut icon" href="./images/favicon.png">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span class="icon fa-leaf"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>Students for Green High Schools</h1>
+								<p>A conference for young environmentalists and activists who are interested in bringing change to their community.</p>
+								<button><a href="https://goo.gl/forms/iljd3GMbV8vONPUu1" style="text-decoration:none">Sign up now</a></button>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="#intro">Info</a></li>
+								<li><a href="#work">Gallery</a></li>
+								<li><a href="#about">About</a></li>
+								<li><a href="#contact">Contact</a></li>
+								<!--<li><a href="#elements">Elements</a></li>-->
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Intro -->
+							<article id="intro">
+								<h2 class="major">Information</h2>
+								<section>
+									<h3 class="major">Location</h3>
+									<center><iframe height="400vh" width="500vw" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJi_OgIgO6j4ARa5kmXo5aJ8c&key=AIzaSyCOUJpOtO-38LVZu-3LtLiaIr0GTG8D5DQ" allowfullscreen></iframe></center>
+								</section>
+								<br />
+								<section>
+									<h3 class="major">Schedule</h3>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Time</th>
+													<th>Description</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>8:00 AM ~ 9:00 AM</td>
+													<td>Registration and Food, Networking</td>
+												</tr>
+												<tr>
+													<td>9:00 AM ~ 9:05 AM</td>
+													<td>Welcome Remarks</td>
+												</tr>
+												<tr>
+													<td>9:05 AM ~ 9:15 AM</td>
+													<td>Icebreakers</td>
+												</tr>
+												<tr>
+													<td>9:15 AM ~ 9:25 AM</td>
+													<td>Presentation from Students of a Sustainable Stanford: Sierra Rosa Garcia</td>
+												</tr>
+												<tr>
+													<td>9:25 AM ~ 10:10 AM</td>
+													<td>Student Presentations:
+														<ul>
+															<li>Saint Francis High School</li>
+															<li>Saratoga High School</li>
+															<li>Homestead High School Green Ops</li>
+															<li>Palo Alto High School: Zero Waste</li>
+															<li>Pinewood Green Coalition</li>
+															<li>Burlingame High School</li>
+														</ul>
+												</tr>
+												<tr>
+													<td>10:10 AM ~ 10:30 AM</td>
+													<td>Break and Networking</td>
+												</tr>
+												<tr>
+													<td>10:30 AM ~ 11:15 AM</td>
+													<td>Student Presentations
+														<ul>
+															<li>San Mateo High School</li>
+															<li>Palo Alto High School: TapOut</li>
+															<li>Mountain View High School: Gardening Club</li>
+															<li>Los Altos High School: Green Team</li>
+															<li>Palo Alto High School Energy Project: Leila Tjiang</li>
+															<li>Saint Francis High School: Fracking Presentation</li>
+														</ul>
+													</td>
+												</tr>
+												<tr>
+													<td>11:15 AM ~ 11:30 AM</td>
+													<td>Green Town Los Altos: Guest Speaker Margie Suozzo</td>
+												</tr>
+												<tr>
+													<td>11:30 AM ~ 11:45 AM</td>
+													<td>Regroup with your Club</td>
+												</tr>
+												<tr>
+													<td>11:45 AM ~ 11:55 AM</td>
+													<td>Closing Remarks</td>
+												</tr>
+												<tr>
+													<td>11:55 AM ~ 12:30 AM</td>
+													<td>Mingle Session: Network, Outreach, and Get Food</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</section>
+							</article>
+
+						<!-- Work -->
+							<article id="work">
+								<h2 class="major">Gallery</h2>
+								<span class="image main">
+									<div class="slideshow-container">
+										<div class="mySlides fade">
+										    <div class="numbertext">1 / 4</div>
+										    <img src="images/img1.jpg" style="width:100%">
+										    <div class="text">Group Photo</div>
+										</div>
+
+										<div class="mySlides fade">
+										    <div class="numbertext">2 / 4</div>
+										    <img src="images/img2.jpg" style="width:100%">
+										    <div class="text">Group Discussion</div>
+										</div>
+
+										<div class="mySlides fade">
+										    <div class="numbertext">3 / 4</div>
+										    <img src="images/img3.jpg" style="width:100%">
+										    <div class="text">Presentation of Ideas</div>
+										</div>
+
+										<div class="mySlides fade">
+										    <div class="numbertext">4 / 4</div>
+										    <img src="images/img4.jpg" style="width:100%">
+										    <div class="text">Discussing with Alumni</div>
+										</div>
+
+										<a class="prev" onclick="plusSlides(-1)" style="text-decoration:none">&#10094;</a>
+										<a class="next" onclick="plusSlides(1)" style="text-decoration:none">&#10095;</a>
+									</div>
+									<br>
+									<div style="text-align:center">
+										<span class="dot" onclick="currentSlide(1)"></span> 
+										<span class="dot" onclick="currentSlide(2)"></span> 
+										<span class="dot" onclick="currentSlide(3)"></span> 
+										<span class="dot" onclick="currentSlide(4)"></span> 
+									</div>
+									<br>
+								</span>
+							</article>
+
+						<!-- About -->
+							<article id="about">
+								<h2 class="major">About</h2>
+								<span class="image main"><img src="images/group.jpg" alt="" /></span>
+								<center><h4>A CONFERENCE DESIGNED BY HIGH SCHOOLERS, FOR HIGH SCHOOLERS</h4></center>
+								<p>Our goal is to help high schoolers effectively implement green projects and foster a culture of environmental responsibility at their schools.  With our student focused model, we aim to help clubs brainstorm feasible and sustainable projects.</p>
+								<p>The format of the conference is pretty simple: We will see some projects that have worked at high schools in the past, split up your team members to let them learn about different areas to 'green your school', and then let them regroup with the members from your school to make a plan for this school year. We hope that your club can use the new-found information from this conference to further "green" your school.</p>
+								<h4>Our Focus:</h4>
+								<ul>
+									<li>Reducing Carbon Emissions/Green Commute</li>
+									<li>Increasing Publicity and Outreach</li>
+									<li>Recycling and Compost</li>
+									<li>Renewable Energy on Campus</li>
+								</ul>
+								<section>
+									<h3 class="major">Sponsors</h3>
+									<img class="image fit" src="images/google.jpg">
+									<a href="https://www.stickermule.com/supports/you"><img class="image fit" src="images/sticker-mule-logo-light.png"/></a>
+									<img class="image" style="width:50%" src="images/greentown_los_altos.png"><img class="image" style="width:40%;display:inline-block;float:right" src="images/california_water_service.png">
+
+								</section>
+							</article>
+
+						<!-- Contact -->
+							<article id="contact">
+								<h2 class="major">Contact</h2>
+								<form method="post" action="#">
+									<div class="field half first">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" />
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email" />
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="4"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send Message" class="special" /></li>
+										<li><input type="reset" value="Reset" /></li>
+									</ul>
+								</form>
+								<ul class="icons">
+									<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+								</ul>
+							</article>
+
+						<!-- Elements -->
+							<article id="elements">
+								<h2 class="major">Elements</h2>
+
+								<section>
+									<h3 class="major">Text</h3>
+									<p>This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
+									This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
+									This is <u>underlined</u> and this is code: <code>for (;;) { ... }</code>. Finally, <a href="#">this is a link</a>.</p>
+									<hr />
+									<h2>Heading Level 2</h2>
+									<h3>Heading Level 3</h3>
+									<h4>Heading Level 4</h4>
+									<h5>Heading Level 5</h5>
+									<h6>Heading Level 6</h6>
+									<hr />
+									<h4>Blockquote</h4>
+									<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan faucibus. Vestibulum ante ipsum primis in faucibus lorem ipsum dolor sit amet nullam adipiscing eu felis.</blockquote>
+									<h4>Preformatted</h4>
+									<pre><code>i = 0;
+
+while (!deck.isInOrder()) {
+    print 'Iteration ' + i;
+    deck.shuffle();
+    i++;
+}
+
+print 'It took ' + i + ' iterations to sort the deck.';</code></pre>
+								</section>
+
+								<section>
+									<h3 class="major">Lists</h3>
+
+									<h4>Unordered</h4>
+									<ul>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Alternate</h4>
+									<ul class="alt">
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Ordered</h4>
+									<ol>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis viverra.</li>
+										<li>Felis enim feugiat.</li>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis lorem.</li>
+										<li>Felis enim et feugiat.</li>
+									</ol>
+									<h4>Icons</h4>
+									<ul class="icons">
+										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+									</ul>
+
+									<h4>Actions</h4>
+									<ul class="actions">
+										<li><a href="#" class="button special">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions vertical">
+										<li><a href="#" class="button special">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Table</h3>
+									<h4>Default</h4>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+
+									<h4>Alternate</h4>
+									<div class="table-wrapper">
+										<table class="alt">
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+								</section>
+
+								<section>
+									<h3 class="major">Buttons</h3>
+									<ul class="actions">
+										<li><a href="#" class="button special">Special</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button">Default</a></li>
+										<li><a href="#" class="button small">Small</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button special icon fa-download">Icon</a></li>
+										<li><a href="#" class="button icon fa-download">Icon</a></li>
+									</ul>
+									<ul class="actions">
+										<li><span class="button special disabled">Disabled</span></li>
+										<li><span class="button disabled">Disabled</span></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Form</h3>
+									<form method="post" action="#">
+										<div class="field half first">
+											<label for="demo-name">Name</label>
+											<input type="text" name="demo-name" id="demo-name" value="" placeholder="Jane Doe" />
+										</div>
+										<div class="field half">
+											<label for="demo-email">Email</label>
+											<input type="email" name="demo-email" id="demo-email" value="" placeholder="jane@untitled.tld" />
+										</div>
+										<div class="field">
+											<label for="demo-category">Category</label>
+											<div class="select-wrapper">
+												<select name="demo-category" id="demo-category">
+													<option value="">-</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+												</select>
+											</div>
+										</div>
+										<div class="field half first">
+											<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+											<label for="demo-priority-low">Low</label>
+										</div>
+										<div class="field half">
+											<input type="radio" id="demo-priority-high" name="demo-priority">
+											<label for="demo-priority-high">High</label>
+										</div>
+										<div class="field half first">
+											<input type="checkbox" id="demo-copy" name="demo-copy">
+											<label for="demo-copy">Email me a copy</label>
+										</div>
+										<div class="field half">
+											<input type="checkbox" id="demo-human" name="demo-human" checked>
+											<label for="demo-human">Not a robot</label>
+										</div>
+										<div class="field">
+											<label for="demo-message">Message</label>
+											<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" class="special" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</form>
+								</section>
+
+							</article>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">Copyright &copy; 2017 LAHS Green Team | Website created by Kung-Min Lin in association with <a href="https://lahs.club">LAHS Hack Club</a> using <a href="https://html5up.net">HTML5 UP</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script> 
+			<script>
+
+				$(document).ready(function() {
+					//window.alert("Website under temporary construction.");
+				});
+
+				var slideIndex = 1;
+				showSlides(slideIndex);
+
+				function plusSlides(n) {
+				  showSlides(slideIndex += n);
+				}
+
+				function currentSlide(n) {
+				  showSlides(slideIndex = n);
+				}
+
+				function showSlides(n) {
+				  var i;
+				  var slides = document.getElementsByClassName("mySlides");
+				  var dots = document.getElementsByClassName("dot");
+				  if (n > slides.length) {slideIndex = 1} 
+				  if (n < 1) {slideIndex = slides.length}
+				  for (i = 0; i < slides.length; i++) {
+				      slides[i].style.display = "none"; 
+				  }
+				  for (i = 0; i < dots.length; i++) {
+				      dots[i].className = dots[i].className.replace(" active", "");
+				  }
+				  slides[slideIndex-1].style.display = "block"; 
+				  dots[slideIndex-1].className += " active";
+				}
+			</script>
+	</body>
+</html>
